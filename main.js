03565ac80dcf75d4a056eafd857b1c58a559c7f6
Nguyen Duyen <s2200128@BC-C02GR0BVQ6LR-MacBook-Air.local>
2022-02-17 16:58:16 +0200

    init commit

---
+++ main.js
@@ -0,0 +1,43 @@
+// Animate progress bars
+const progress_bars = document.querySelectorAll(".progress");
+
+progress_bars.forEach((bar) => {
+  const { size } = bar.dataset;
+  bar.style.width = `${size}%`;
+});
+
+// VARIABLES
+const header = document.querySelector("header");
+const burgerBtn = document.querySelector("#burger-btn");
+const navMobile = document.querySelector("nav");
+const navLinks = document.querySelectorAll("ul li a");
+// open & close nav mobile
+const showMobileMenu = () => {
+  navLinks.forEach((link) => {
+    link.addEventListener("click", showMobileMenu);
+  });
+  navMobile.classList.toggle("responsive");
+};
+
+burgerBtn.addEventListener("click", showMobileMenu);
+
+// Toggle showing back to top button and toggle header bg on scroll
+
+window.onscroll = () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    header.classList.add("bgcolorchange");
+    toTopButton.style.display = "block";
+  } else {
+    header.classList.remove("bgcolorchange");
+    toTopButton.style.display = "none";
+  }
+};
+
+// Back to top function
+const toTopButton = document.querySelector("#backToTop");
+
+const backToTop = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
+toTopButton.addEventListener("click", backToTop);
